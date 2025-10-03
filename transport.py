a3ba4c1793789d91e827efee05ed9ba9a4eec030
nstssss <nastastrumanowa@gmail.com>
2025-10-04 00:27:47 +0300

    абстрактный класс

---
+++ transport.py
@@ -0,0 +1,24 @@
+import abc
+from abc import ABC, abstractmethod
+"""абстрактный класс транспорта"""
+class Transport(ABC):
+    # def __init__(self, distance, passengers, ticket_price):
+    #     self.distance = distance
+    #     self.passengers = passengers
+    #     self.ticket_price = ticket_price
+    #     self.average_speed = self.get_speed()
+    @abstractmethod
+    def get_speed(self):
+        pass
+
+    @abc.abstractmethod
+    def calculate_cost(self):
+        pass
+
+    @abc.abstractmethod
+    def calculate_time(self):
+        pass
+
+    @abc.abstractmethod
+    def calculate_passеger_flow(self):
+        pass
